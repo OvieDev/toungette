93dd2013c99e16bcd989c76ea54f98f8ed06d4a8
OvieDev <swiderskijan40@gmail.com>
2022-09-11 00:38:45 +0200

    Updated README.md

--- source/Translator.php
+++ source/Translator.php
@@ -5,9 +5,20 @@ namespace toungette;
 class Translator
 {
     private $template_path;
+    private $page_template;
     private $lang;
 
-    public function __construct($template, $lang=null) {
-
+    public function __construct($template, $page, $lang=null) {
+        $this->template_path = $template;
+        $this->page_template = $page;
+        if (isset($lang)) {
+            $this->lang = $lang;
+            if (isset($_GET['lang'])) {
+                $this->lang = substr($_GET['lang'], 0, 2);
+            }
+        }
+        else {
+            $this->lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        }
     }
 }
